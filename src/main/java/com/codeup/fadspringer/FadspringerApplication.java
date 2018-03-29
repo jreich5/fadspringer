5ed0867c9c9666b3055a9791fcb537382ce9b8af
Justin Reich <justinreich.dev@gmail.com>
2018-03-29 14:46:09 -0500

    Inital commit

---
+++ src/main/java/com/codeup/fadspringer/FadspringerApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.fadspringer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FadspringerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FadspringerApplication.class, args);
+	}
+}
